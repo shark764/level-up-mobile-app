5329d9de77e076d822530ff3b016255b8aad936a
focuswrivas <William.Rivas@focusservices.com>
2021-04-20 22:42:23 -0600

    Build PasswordConfirmation screen, commit and push all changes and MR

--- src/screens/ForgotPassword/index.ts
+++ src/screens/ForgotPassword/index.ts
@@ -1,4 +1,7 @@
 import ForgotPassword from "./ForgotPasswordS1";
 import ForgotPasswordVerification from "./ForgotPasswordS2";
-export { ForgotPassword, ForgotPasswordVerification } 
+import ForgotPasswordReset from "./ForgotPasswordS3";
+import ForgotPasswordConfirm from "./ForgotPasswordS4";
+
+export { ForgotPassword, ForgotPasswordVerification, ForgotPasswordReset, ForgotPasswordConfirm } 
 
