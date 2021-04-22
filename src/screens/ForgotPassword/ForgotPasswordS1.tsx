9851e1bac19712f31d80c25dd5c108eda420de4c
Irvin Sandoval <irvandoval@gmail.com>
2021-04-21 21:30:41 -0600

    add new design

--- src/screens/ForgotPassword/ForgotPasswordS1.tsx
+++ src/screens/ForgotPassword/ForgotPasswordS1.tsx
@@ -10,19 +10,29 @@ import ForgotPasswordBottom from './Components/ForgotPasswordBottom';
 const ForgotPassword = () => {
   return (
     <SafeAreaView style={[styles.fpContainer]}>
-    <ScrollView>
-      <View style={styles.main}>
-        <View style={styles.backSection}>
-          <Text style={styles.backText}> Back </Text> 
+      <ScrollView>
+        <View style={styles.main}>
+          <View style={styles.backSection}>
+            <Icon style={styles.backIcon} name='chevron-left' size={26} />
+            <Text style={styles.backText}> Back</Text>
+          </View>
+          <View>
+            <ForgotPasswordHeader
+              viewStyle={styles.containerText}
+              textHeader={styles.textHeader}
+              textSubHeader={styles.textSubHeader}
+            />
+            <ForgotPasswordInput viewStyle={styles.containerInputs} />
+            <ForgotPasswordBottom
+              viewStyle={styles.containerText}
+              buttonStyle={styles.button}
+              navigationButton={'SignUp'}
+              buttonTitleStyle={styles.buttonTitle}
+            />
+          </View>
         </View>
-        <View>
-          <ForgotPasswordHeader viewStyle={styles.containerText} textHeader={styles.textHeader} textSubHeader={styles.textSubHeader}/>
-          <ForgotPasswordInput viewStyle={styles.containerInputs}/>
-          <ForgotPasswordBottom viewStyle={styles.containerText} buttonStyle={styles.button} navigationButton={'ForgotPasswordVerification'} buttonTitleStyle={styles.buttonTitle}/>
-        </View>
-        </View>  
-    </ScrollView>
-  </SafeAreaView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
